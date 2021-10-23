2471a7b20dcb33ede4c2e35145bd052e7eff3c90
adammakai8 <adammakai8@gmail.com>
2021-10-23 17:09:12 +0200

    segmentation.py implementációjának elkezdése

---
+++ src/segmentation.py
@@ -0,0 +1,31 @@
+""" Szegmentálást és a szegmensek áramlását megvalósító függvények """
+
+import numpy as np
+import cv2
+from os import listdir
+from os import mkdir
+
+prep_dir = '../res/preprocessed/'
+seg_dir = '../res/segmented/'
+
+
+def create_and_save_segments_from_preprocessed():
+    """
+    Az előfeldolgozott képek címkéi alapján kivágja a dobókockákat és elmenti a res/segmented mappába.
+    Tesztelési célokra használatos.
+    """
+    for pic in listdir(prep_dir):
+        image = cv2.imread(f'{prep_dir}{pic}', cv2.IMREAD_GRAYSCALE)
+        labels = [int(item) for item in pic.split('.')[0].split('_')]
+        dice1 = image[labels[1]:labels[3], labels[0]:labels[2]]
+        dice2 = image[labels[5]:labels[7], labels[4]:labels[6]]
+        dirname = pic.split(".")[0]
+        try:
+            mkdir(f'{seg_dir}{dirname}')
+        except FileExistsError:
+            continue
+        cv2.imwrite(f'{seg_dir}{dirname}/{pic.split(".")[0]}_1.png', dice1)
+        cv2.imwrite(f'{seg_dir}{dirname}/{pic.split(".")[0]}_2.png', dice2)
+
+
+create_and_save_segments_from_preprocessed()
