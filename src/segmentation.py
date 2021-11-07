e7db779c11384aa941aec52dcee66b27adc0939a
adammakai8 <adammakai8@gmail.com>
2021-11-07 18:59:00 +0100

    az élesben használt szegmentáló implementálása és kódoptimalizálás

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,6 +1,5 @@
 """ Szegmentálást és a szegmensek áramlását megvalósító függvények """
 
-import numpy as np
 import cv2
 from os import listdir
 from os import mkdir
@@ -15,17 +14,24 @@ def create_and_save_segments_from_preprocessed():
     Tesztelési célokra használatos.
     """
     for pic in listdir(prep_dir):
-        image = cv2.imread(f'{prep_dir}{pic}', cv2.IMREAD_GRAYSCALE)
-        labels = [int(item) for item in pic.split('.')[0].split('_')]
-        dice1 = image[labels[1]:labels[3], labels[0]:labels[2]]
-        dice2 = image[labels[5]:labels[7], labels[4]:labels[6]]
         dirname = pic.split(".")[0]
         try:
             mkdir(f'{seg_dir}{dirname}')
         except FileExistsError:
             continue
+        image = cv2.imread(f'{prep_dir}{pic}', cv2.IMREAD_GRAYSCALE)
+        labels = [int(item) for item in pic.split('.')[0].split('_')]
+        dice1 = image[labels[1]:labels[3], labels[0]:labels[2]]
+        dice2 = image[labels[5]:labels[7], labels[4]:labels[6]]
         cv2.imwrite(f'{seg_dir}{dirname}/{pic.split(".")[0]}_1.png', dice1)
         cv2.imwrite(f'{seg_dir}{dirname}/{pic.split(".")[0]}_2.png', dice2)
 
 
-create_and_save_segments_from_preprocessed()
+def create_segment(coordinates, image):
+    dice1 = image[coordinates[1]:coordinates[3], coordinates[0]:coordinates[2]]
+    dice2 = image[coordinates[5]:coordinates[7], coordinates[4]:coordinates[6]]
+    return dice1, dice2
+
+
+# Ha külön akartok szegmenseket kimenteni tesztadatként az alsó sor kommentezzétek ki
+# create_and_save_segments_from_preprocessed()
