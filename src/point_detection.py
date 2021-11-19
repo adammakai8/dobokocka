68232eeb532a88e4d1ba8e0f7cc2001a6583b193
adammakai8 <adammakai8@gmail.com>
2021-11-19 17:50:03 +0100

    Pont detekció elkezdése

---
+++ src/point_detection.py
@@ -0,0 +1,21 @@
+import cv2
+import debug_util as debug
+
+
+# TODO: implementáció folytatása a következőképp (az egyes paraméterek finomhangolhatóak):
+#   - a kontúrokat szűrni méret alapján: képméret harmadánál kisebbek és századrészénél nagyobbak jók
+#   - cirkularitás: perfekt kör esetén 0.08, fogadjuk el a 0.05 felettieket
+#   - ha túl kicsi a kép, ezért nem érzékel egy kört se (túl kockás):
+#       - input kép nagyítása, medián vagy átlag szűrés, majd pontdetektálás újrakezdése
+#   - segédanyag, ha kell: https://www.inf.u-szeged.hu/~tanacs/pyocv/alakjellemzk_szmtsa.html
+def count_points(dice):
+    dice_mask = cv2.adaptiveThreshold(dice, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, -15)
+    # debugoláshoz, ha bele akarsz nézni, mit csinál a képekkel a progi
+    debug.imshowAndPause('dice_mask', dice_mask)
+    contours, hierarchy = cv2.findContours(dice_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    return 0
+
+
+def point_detection(dice1, dice2):
+
+    return count_points(dice1) + count_points(dice2)
